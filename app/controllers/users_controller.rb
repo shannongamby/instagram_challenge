1c5b140fcce812f8505780bb9e64095ae93ae44f
Shannon Gamby <shannongamby@hotmail.com>
2019-01-10 11:35:46 +0000

    added flash success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to '/'
+      flash[:success] = "Thank you for signing up!"
     else
       render :new
     end
