fd178ccac02f9a3142e7f31da88e9e11e8f10dba
Shannon Gamby <shannongamby@hotmail.com>
2019-01-15 20:04:06 +0000

    validating image presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :caption, presence: true, length: { maximum: 300 }
+  validates :caption, presence: true, length: { maximum: 500 }
 
   has_one_attached :image
+  validates :image, presence: true
 end
