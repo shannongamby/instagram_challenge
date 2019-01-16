1e37150c082cdb675b62266bf46b69b3df268272
Shannon Gamby <shannongamby@hotmail.com>
2019-01-16 15:52:21 +0000

    post controller test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,22 +9,32 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  describe "GET #profile" do
+    it "returns http success" do
+      get :profile
+      expect(response).to have_http_status(:success)
+    end
+  end
+
   describe "GET #new" do
+    it "shows new post form if user logged in" do
+      allow(subject).to receive(:logged_in?).and_return(true)
+      get :new
+      expect(response).to have_http_status(:success)
+    end
     it "redirects to login page if user not logged in" do
       get :new
       expect(response).to redirect_to '/login'
     end
   end
 
-  describe 'POST /' do
-      before(:each) do
-        user = FactoryBot.create(:user)
-        stub_current_user(user)
-      end
-    end
-
   # describe "POST #create" do
+  #   let(:user) { double :user }
+  #   let(:main_image) { double :main_image }
   #   it "returns http success" do
+  #     allow(subject).to receive(:current_user).and_return(user)
+  #     allow(subject).to receive(:image).and_return(main_image)
+  #     allow(user).to receive(:id).and_return(1)
   #     post :create, params: { post: { caption: 'Hello, world!' } }
   #     expect(response).to have_http_status(:success)
   #   end
